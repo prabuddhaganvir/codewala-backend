791e7557457ec9d115935bf581df13187394bf5c
prabuddhaganvir <prabuddhapg12@gmail.com>
2025-07-18 15:45:01 +0530

    added new url

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const PORT = process.env.PORT;
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin: "https://codewala-frontend.vercel.app",
+  origin: "https://blogwala.vercel.app",
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
